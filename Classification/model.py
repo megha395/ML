eb1050d350d8392863f094300b0c6d838516b10f
Megha Kumari <55241241+megha395@users.noreply.github.com>
2024-08-10 08:13:16 +0530

    deleted extra line

--- Classification/model.py
+++ Classification/model.py
@@ -7,5 +7,4 @@ model.fit(x_train, y_train)
 ### Prediction of test data
 y_pred = model.predict(x_test)
 print(f"Predicted data is: \n Test data: \n {x_test} \n Prediction:  \n {y_pred}")
-
 #testing some changes
